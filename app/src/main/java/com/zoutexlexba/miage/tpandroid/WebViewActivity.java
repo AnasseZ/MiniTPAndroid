eae07e11392d5657271824304dc4885986e410c3
Anasse Zougarh <zougarh.anasse@gmail.com>
2017-10-16 14:54:03 +0200

    Ajout d'une webview a l'activity 3

--- app/src/main/java/com/zoutexlexba/miage/tpandroid/WebViewActivity.java
+++ app/src/main/java/com/zoutexlexba/miage/tpandroid/WebViewActivity.java
@@ -2,12 +2,19 @@ package com.zoutexlexba.miage.tpandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class WebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_view);
+
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.loadUrl("https://developer.android.com/index.html");
+
+        setContentView(webView);
     }
 }
