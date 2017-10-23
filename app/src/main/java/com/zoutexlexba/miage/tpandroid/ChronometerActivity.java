b389ee7b69899f498cbb531576ea6584f6e86ad4
Anasse Zougarh <zougarh.anasse@gmail.com>
2017-10-23 12:20:17 +0200

    Les boutons sont desactives selon l'etat du chronometre

--- app/src/main/java/com/zoutexlexba/miage/tpandroid/ChronometerActivity.java
+++ app/src/main/java/com/zoutexlexba/miage/tpandroid/ChronometerActivity.java
@@ -5,6 +5,7 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 
 public class ChronometerActivity extends AppCompatActivity {
@@ -22,20 +23,34 @@ public class ChronometerActivity extends AppCompatActivity {
         chronometer = (Chronometer) findViewById(R.id.chronometer);
         chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
         chronometer.start();
+
+        this.enableButton(R.id.playChrono,false);
+        this.enableButton(R.id.stopChrono,true);
     }
 
     public void stopChrono(View view) {
         timeWhenStopped = chronometer.getBase() - SystemClock.elapsedRealtime();
         chronometer.stop();
+
+        this.enableButton(R.id.playChrono,true);
+        this.enableButton(R.id.stopChrono,false);
     }
 
     public void resetChrono(View view) {
         chronometer.setBase(SystemClock.elapsedRealtime());
         timeWhenStopped = 0;
+
+        this.enableButton(R.id.playChrono,true);
+        this.enableButton(R.id.stopChrono,true);
     }
 
     public void displayWebViewActivity(View view) {
         Intent intent = new Intent(this,WebViewActivity.class);
         startActivity(intent);
+    }
+
+    private void enableButton(int id, boolean enable) {
+        Button button = (Button) findViewById(id);
+        button.setEnabled(enable);
     }
 }
